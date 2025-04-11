193749d5bd25d793e8d6fe374a85c8bfca99b709
Santi Hernandez <santi@santihdzs.com>
2025-04-11 09:57:38 -0600

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+class Video{
+private:
+protected:
+public:
+};
+
+class Movies : protected Video {
+private:
+public:
+};
+
+class Series : protected Video {
+private:
+public:
+};
+
+int main(){
+
+    return 0;
+}
